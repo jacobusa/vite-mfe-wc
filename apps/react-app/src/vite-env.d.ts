eac53fb2772f112f943853a9e049b8b656cd25b9
Kobus <jacobusbad@gmail.com>
2024-02-20 23:49:26 -0700

    fix type errors

--- apps/react-app/src/vite-env.d.ts
+++ apps/react-app/src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
-export {};
-declare global {
-  interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    navigateTo: any;
-  }
-}
+// export {};
+// declare global {
+//   interface Window {
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//     navigateTo: any;
+//   }
+// }
--- apps/shell/src/vite-env.d.ts
+++ apps/shell/src/vite-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
 
-export {};
 declare namespace JSX {
   interface IntrinsicElements {
     "react-app": React.DetailedHTMLProps<
@@ -8,12 +7,5 @@ declare namespace JSX {
       HTMLElement
     >;
     // Add any additional props expected by 'react-app' as needed
-  }
-}
-
-declare global {
-  interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    navigateTo: any;
   }
 }
